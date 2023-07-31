05f23977cc7115444a93f5379931768e3d77de35
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-07-31 19:04:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const single = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const double = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const splitNumber = number.toString().split('').map(x => +x)
+    if (number <= 19) {
+        return single[number]
+    }
+    else if (number < 100) {
+        return double[splitNumber[0]] + (splitNumber[1] != 0 ? ' ' + single[splitNumber[1]] : '')
+    } else if (number < 1000) {
+        if (splitNumber[1] == 0 && splitNumber[2] == 0) {
+            return single[splitNumber[0]] + ' hundred'
+        } else if (splitNumber[1] == 1 || splitNumber[1] == 0) {
+            return single[splitNumber[0]] + ' hundred ' + single[number % 100]
+        } else {
+            return single[splitNumber[0]] + ' hundred ' + double[splitNumber[1]] + (splitNumber[2] != 0 ? ' ' + single[splitNumber[2]] : '')
+        }
+    } else {
+        return 'More than a thousand'
+    }
 }
