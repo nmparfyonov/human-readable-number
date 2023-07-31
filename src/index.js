e2378e90e67513cea096b6c4f66c9e03fb79eac6
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-07-31 22:11:59 +0300

    fix: remove array

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 module.exports = function toReadable(number) {
     const single = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     const double = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-    const splitNumber = number.toString().split('').map(x => +x)
     if (number <= 19) {
         return single[number]
     }
     else if (number < 100) {
-        return double[splitNumber[0]] + (splitNumber[1] != 0 ? ' ' + single[splitNumber[1]] : '')
+        return double[Math.floor(number / 10)] + (number % 10 != 0 ? ' ' + single[number % 10] : '')
     } else if (number < 1000) {
-        if (splitNumber[1] == 0 && splitNumber[2] == 0) {
-            return single[splitNumber[0]] + ' hundred'
-        } else if (splitNumber[1] == 1 || splitNumber[1] == 0) {
-            return single[splitNumber[0]] + ' hundred ' + single[number % 100]
+        if (number % 100 == 0) {
+            return single[number / 100] + ' hundred'
+        } else if (number % 100 < 20) {
+            return single[Math.floor(number / 100)] + ' hundred ' + single[number % 100]
         } else {
-            return single[splitNumber[0]] + ' hundred ' + double[splitNumber[1]] + (splitNumber[2] != 0 ? ' ' + single[splitNumber[2]] : '')
+            return single[Math.floor(number / 100)] + ' hundred ' + double[Math.floor(number % 100 / 10)] + (number % 10 != 0 ? ' ' + single[number % 10] : '')
         }
     } else {
-        return 'More than a thousand'
+        return 'Thousand and more'
     }
 }
